6ee1b0a7e4790a32b9067d84a7702627c6c406f2
Snaggletoof <brentsoffey@gmail.com>
2018-01-31 16:07:46 -0800

    Add files via upload

---
+++ hi_sam.py
@@ -0,0 +1,12 @@
+#I'm learnding! *picture Ralph Wiggums*
+
+sams_penis = "8===D"
+brents_penis = "8======D"
+brents_pleasure_factor = 2
+sams_pleasure_factor = 1
+
+print "Sam's penis is %s thiiiiis big" % sams_penis
+print "But Brent's penis is %s thiiiiiiis big" % brents_penis
+print "So, because it is %s that Brent can please Sam more than the other way around..." % (brents_pleasure_factor >= sams_pleasure_factor)
+print "Brent is the dominant penetrator in the relationship."
+
